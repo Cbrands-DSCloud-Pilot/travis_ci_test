9243e41dcb4ac719611c22b0d96d89e1b6023106
jon-novak <jon.novak@cbrands.com>
2017-09-22 15:40:16 -0500

    Corrected issue

--- tests_2.py
+++ tests_2.py
@@ -4,7 +4,7 @@ from hello_world import hello_world
 class TestHello(unittest.TestCase):
     
     def test_hello(self):
-        self.assertEqual(hello_world() , 'hello_world1') 
+        self.assertEqual(hello_world() , 'hello_world') 
 
 if __name__ == '__main__':
     unittest.main()  
