1168bb8496d70fe996c7260d05bfe5eb0e45132c
jon-novak <jon.novak@cbrands.com>
2017-09-22 15:22:17 -0500

    Added tests.py

---
+++ tests.py
@@ -0,0 +1,10 @@
+import unittest
+from hello_world import hello_world
+
+class TestHello(unittest.TestCase):
+    
+    def test_hello(self):
+        self.assertEqual(hello_world() , 'hello_world') 
+
+if __name__ == '__main__':
+    unittest.main()  
